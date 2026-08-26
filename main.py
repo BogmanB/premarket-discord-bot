c75c4aee6b5c5be3c9e78455687590a6b15940c4
BogmanB <thebeny123@gmail.com>
2026-08-26 16:13:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
 import os
 import requests
 
-webhook_url = os.environ.get("DISCORD_WEBHOOK")
+API_KEY = os.environ["FINNHUB_API_KEY"]
+WEBHOOK = os.environ["DISCORD_WEBHOOK"]
 
-if not webhook_url:
-    print("DISCORD_WEBHOOK není nastaven.")
-else:
-    response = requests.post(
-        webhook_url,
-        json={"content": "🚀 Premarket bot funguje!"},
-        timeout=15
-    )
+symbol = "NVDA"
 
-    print(response.status_code)
+url = "https://finnhub.io/api/v1/quote"
+
+response = requests.get(
+    url,
+    params={
+        "symbol": symbol,
+        "token": API_KEY
+    },
+    timeout=15
+)
+
+data = response.json()
+
+message = (
+    f"📊 TEST DATA — {symbol}\n\n"
+    f"💰 Cena: ${data.get('c')}\n"
+    f"📈 Změna: {data.get('dp')} %\n"
+    f"⬆️ High: ${data.get('h')}\n"
+    f"⬇️ Low: ${data.get('l')}\n"
+)
+
+requests.post(
+    WEBHOOK,
+    json={"content": message},
+    timeout=15
+)
+
+print(data)
